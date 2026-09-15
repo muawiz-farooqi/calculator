eb850e6d3ed68dae7723d02a0a5836ac7a7c019d
Muhammad Muawiz Farooqi <muawizfarooqi@yahoo.com>
2026-09-15 17:44:49 +0400

    added formatting display, decimal, toggle sign, divide by zero

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let first_num = null;
 let second_num = null;
 let curr_operator = null;
 let equalsClicked = false;
+const MAX_DIGITS = 8;
 
 const display = document.querySelector("#display");
 
@@ -37,6 +38,14 @@ numbers.forEach((number) => {
     });
 });
 
+// .
+const decimal = document.querySelector("#decimal");
+decimal.addEventListener("click", () => {
+    if (!display.value.includes(".")) {
+        display.value = String(display.value) + ".";
+    }
+});
+
 // + - * / =
 const operators = document.querySelectorAll(".operator");
 operators.forEach((operator) =>
@@ -47,11 +56,22 @@ operators.forEach((operator) =>
             curr_operator = operator.textContent;
         } else {
             second_num = Number(display.value);
+
+            // catch divide by zero
+            if (curr_operator === "/" && second_num == 0) {
+                display.value = "💥💥💥💥";
+                first_num = null;
+                second_num = null;
+                curr_operator = null;
+                return;
+            }
+
             first_num = operate(curr_operator, first_num, second_num);
             curr_operator = operator.textContent;
+
             if (curr_operator === "=") {
                 equalsClicked = true;
-                display.value = first_num;
+                display.value = formatCalculatorDisplay(first_num);
                 first_num = null;
                 second_num = null;
                 curr_operator = null;
@@ -69,9 +89,10 @@ clear.addEventListener("click", () => {
     first_num = null;
     second_num = null;
     curr_operator = null;
+    equalsClicked = false;
 });
 
-// backspace
+// Delete
 const backspace = document.querySelector("#delete");
 backspace.addEventListener("click", () => {
     if (display.value.length === 1) {
@@ -80,3 +101,24 @@ backspace.addEventListener("click", () => {
         display.value = display.value.slice(0, -1);
     }
 });
+
+// toggle sign
+const signToggle = document.querySelector("#toggle");
+signToggle.addEventListener("click", () => {
+    if (display.value != 0) {
+        display.value *= -1;
+    }
+});
+
+// formatting display
+function formatCalculatorDisplay(value) {
+    if (value === "0" || value === "0.") return value;
+
+    let num = Number(value);
+    if (isNaN(num)) return value;
+
+    let formatted = num.toPrecision(MAX_DIGITS);
+    let clean = Number(formatted);
+    return clean.toString();
+    console.log(formatted, clean);
+}
